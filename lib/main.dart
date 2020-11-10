9fcc7c2ca0aded178f13456d35445b0c4001dc68
BrunoMCerqueira <brunocerqueirasd@gmail.com>
2020-11-10 01:50:13 -0300

    Create xylophone to play audios.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  Expanded buildKey(Color color, int elementNumber) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          final player = AudioCache();
+
+          player.play('note$elementNumber.wav');
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Text(nouns.first),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.grey[900], 1),
+              buildKey(Colors.grey[800], 2),
+              buildKey(Colors.grey[700], 3),
+              buildKey(Colors.grey[600], 4),
+              buildKey(Colors.grey[500], 5),
+              buildKey(Colors.grey[400], 6),
+              buildKey(Colors.grey[300], 7),
+            ],
           ),
         ),
       ),
